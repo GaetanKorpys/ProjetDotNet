15924cc0f35f2b82cf5ab52ea986d70b30b3c7a3
Gaetan <gaetan.korpys@hotmail.fr>
2023-04-26 22:03:25 +0200

    Add DateConverter Class + Fix display numéro d'enquête : dd/mm/yyyy

---
+++ Converters/DateConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjetDotNet.Converters
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime date)
+            {
+                return date.ToString("dd/MM/yyyy");
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (DateTime.TryParse(value as string, out DateTime date))
+            {
+                return date;
+            }
+
+            return value;
+        }
+    }
+}
