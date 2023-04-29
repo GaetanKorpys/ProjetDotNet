cbf31e6166300059d0bd9c1541b7c11180344034
Gaetan <gaetan.korpys@hotmail.fr>
2023-04-29 15:20:30 +0200

    New ProofPicture Class in Model  + Add List Visit in VisitView

---
+++ Models/ProofPicture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDotNet.Models
+{
+    public class ProofPicture
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProofPictureId { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public int VisitId { get; set; }
+        public Visit Visit { get; set; }
+    }
+}
